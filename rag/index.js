291dded279c82dbfba5b0a23123d41ef41685bbd
akshatsisodia <sisodiaakshat77@gmail.com>
2026-03-29 22:54:11 +0530

    feat: implement PDF parsing and embedding pipeline using LangChain & Mistral AI

---
+++ rag/index.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+import { PDFParse } from "pdf-parse";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import "dotenv";
+import { config } from "dotenv";
+config();
+
+const embeddings = new MistralAIEmbeddings({
+    model:"mistral-embed",
+    apiKey: process.env.MISTRAL_API_KEY
+})
+
+let dataBuffer = fs.readFileSync("./story.pdf");
+
+const parser = new PDFParse({
+    data:dataBuffer
+});
+
+const data = await parser.getText()
+
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize:500,
+    chunkOverlap:0
+})
+
+const chunks = await splitter.splitText(data.text);
+
+const docs = await embeddings.embedDocuments(chunks);
+
+console.log(docs);
+
