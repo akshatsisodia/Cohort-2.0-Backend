704fe11b68c94a40c661d3382b82d845cb489052
akshatsisodia <sisodiaakshat77@gmail.com>
2026-04-07 22:39:06 +0530

    feat: implement Google OAuth using passport-google-oauth20
    - Added GoogleStrategy configuration
    - Setup authentication routes (/auth/google, callback)
    - Integrated passport initialization
    - Added environment variables for client credentials

--- google-Oauth/server.js
+++ google-Oauth/server.js
@@ -1,7 +1,39 @@
 import { config } from "dotenv";
 import app from "./src/app.js";
+import passport from "passport";
+import morgan from "morgan";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
 
 config();
+
+app.use(morgan("dev"));
+
+app.use(passport.initialize());
+
+passport.use(new GoogleStrategy({
+    clientID:process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL:"/auth/google/callback"
+},(_,__,profile, done)=>{
+    return done(null, profile)
+}))
+
+
+app.get('/',(req, res)=>{
+    res.send("hello world");
+})
+
+app.get("/auth/google", passport.authenticate("google", {scope: ["profile","email"]}))  
+
+app.get("/auth/google/callback",
+    passport.authenticate("google", { 
+        session:false,
+        failureRedirect: "/"} ),
+    (req,res)=>{
+        console.log(req.user);
+        res.send("Google Authentication Successfull.")
+    }
+ )
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
